495c2da194703a2de3f616c34814892dd665298f
Artahacking <faizarta9@gmail.com>
2025-07-02 16:32:10 +0700

    fix: add dynamic flag to API routes and fix prisma queries

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Separator } from "@/components/ui/separator";
 import { Info } from "./_components/info";
 import { BoardList } from "./_components/board-list";
